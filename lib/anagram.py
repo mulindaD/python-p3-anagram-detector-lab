e40cf463e218c4ba6c765c0fb5c81fd037e7ccbb
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-10-02 18:55:51 +0300

    Complete anagrams lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,30 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word="word"):
+        self._word = word
+
+    @property
+    def word(self):
+        return self._word
+    
+    @word.setter
+    def word(self, word):
+        if type(word) is str:
+            self._word = word
+        else:
+            print("Please enter a valid word!")
+
+    def match(self, list_of_anagrams):
+        anagrams = []
+
+        for word in list_of_anagrams:
+            if sorted([char for char in self._word]) == sorted([char for char in word]):
+                anagrams.append(word)
+            
+        return anagrams
+        
+        
+
+
+    
